d34cb7a882b817272d46f3a6587759dede4fead7
neasher1 <neasher75@gmail.com>
2023-08-03 15:55:02 +0600

    firebase create user

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAdOYhEMYwRz2x6pgNSGkKQcPf-cRs4gUk",
+    authDomain: "nextauth-89a46.firebaseapp.com",
+    projectId: "nextauth-89a46",
+    storageBucket: "nextauth-89a46.appspot.com",
+    messagingSenderId: "956429303851",
+    appId: "1:956429303851:web:745bed9c4321f08dae6a6e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,22 @@
+import auth from "@/firebase/firebase.auth";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
+import { useAuthState } from "react-firebase-hooks/auth";
 const HomePage = () => {
+  //next auth
   const { data: session } = useSession()
+
+  //firebase
+  const [user, loading, error] = useAuthState(auth);
+  console.log(user);
   return (
     <div>
       <Head>
         <title>Next Auth</title>
       </Head>
       <h1 style={{ textAlign: "center", marginTop: "10%" }}>Welcome To Next Auth Home Page</h1>
-      <h2 style={{ textAlign: "center" }}>user: {session?.user?.name}</h2>
+      {/* <h2 style={{ textAlign: "center" }}>user: {session?.user?.name}</h2> */}
+      <h2 style={{ textAlign: "center" }}>user: {user?.email}</h2>
     </div>
   );
 };
